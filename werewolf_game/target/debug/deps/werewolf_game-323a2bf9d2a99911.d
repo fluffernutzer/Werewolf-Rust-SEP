048582ee7b054ebc49c40161f8f855173d5d75bc
Martin Wilhelm <mg-wilhelm@outlook.com>
2025-12-11 16:48:27 +0100

    Initialer Commit. Nothing to see here

---
+++ werewolf_game/target/debug/deps/werewolf_game-323a2bf9d2a99911.d
@@ -0,0 +1,5 @@
+/Users/fluffernutzer/Documents/Rust SEP/Werewolf-Rust-SEP/werewolf_game/target/debug/deps/werewolf_game-323a2bf9d2a99911.d: src/main.rs
+
+/Users/fluffernutzer/Documents/Rust SEP/Werewolf-Rust-SEP/werewolf_game/target/debug/deps/libwerewolf_game-323a2bf9d2a99911.rmeta: src/main.rs
+
+src/main.rs:
